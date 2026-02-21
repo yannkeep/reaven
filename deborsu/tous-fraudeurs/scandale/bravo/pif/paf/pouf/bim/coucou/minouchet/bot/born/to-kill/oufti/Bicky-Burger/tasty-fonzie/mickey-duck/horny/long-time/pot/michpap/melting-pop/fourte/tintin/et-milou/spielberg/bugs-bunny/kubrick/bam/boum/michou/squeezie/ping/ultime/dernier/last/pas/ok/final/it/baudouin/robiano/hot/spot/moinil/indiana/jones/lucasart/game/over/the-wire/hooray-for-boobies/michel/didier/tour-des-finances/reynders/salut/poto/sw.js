9bfae80f56618a02152f2ac4aa3b0f18a52c4969
#!BiM <yannkeep@users.noreply.github.com>
2026-02-21 02:31:46 +0100

    Add files via upload

---
+++ deborsu/tous-fraudeurs/scandale/bravo/pif/paf/pouf/bim/coucou/minouchet/bot/born/to-kill/oufti/Bicky-Burger/tasty-fonzie/mickey-duck/horny/long-time/pot/michpap/melting-pop/fourte/tintin/et-milou/spielberg/bugs-bunny/kubrick/bam/boum/michou/squeezie/ping/ultime/dernier/last/pas/ok/final/it/baudouin/robiano/hot/spot/moinil/indiana/jones/lucasart/game/over/the-wire/hooray-for-boobies/michel/didier/tour-des-finances/reynders/salut/poto/sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'affaire-reynders-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,700;0,900;1,400&family=Source+Serif+4:ital,wght@0,300;0,400;0,600;0,700;1,400&family=JetBrains+Mono:wght@400;700&family=DM+Sans:wght@400;500;700&display=swap'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
